0f02d9c426e8312a13d6498ccfdfbd95ced0fe88
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2022-12-03 13:26:47 +0600

    feat: start working on applying fft

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 import xlrd
 import numpy as np
 
-def __normalize_excel_data_from_file(data):
-    return list(map(lambda row: row[:2], data))
-
 def read_from_excel_file(Workbook_path, sheet_name):
     book = xlrd.open_workbook(Workbook_path)
     sheet = book.sheet_by_name(sheet_name)
     data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
-    return __normalize_excel_data_from_file(data)
+    return __normalize_excel_data(data)
+
+def __normalize_excel_data(data):
+    return __sort_by_x_coordinates(__remove_columns(__remove_rows(data)))
+
+def __remove_rows(data):
+    return data[1:]
+
+def __remove_columns(data):
+    return list(map(lambda row: {'x': row[0], 'y': row[1]}, data))
+
+def __sort_by_x_coordinates(data):
+    return sorted(data, key=lambda d: d['x'])
 
 def FFT_vectorized(x):
     x = np.asarray(x, dtype=float)
@@ -19,17 +28,13 @@ def FFT_vectorized(x):
     if np.log2(N) % 1 > 0:
         raise ValueError("size of x must be a power of 2")
 
-    # N_min here is equivalent to the stopping condition above,
-    # and should be a power of 2
     N_min = min(N, 32)
-    
-    # Perform an O[N^2] DFT on all length-N_min sub-problems at once
+
     n = np.arange(N_min)
     k = n[:, None]
     M = np.exp(-2j * np.pi * n * k / N_min)
     X = np.dot(M, x.reshape((N_min, -1)))
 
-    # build-up each level of the recursive calculation all at once
     while X.shape[0] < N:
         X_even = X[:, :X.shape[1] / 2]
         X_odd = X[:, X.shape[1] / 2:]
@@ -41,4 +46,11 @@ def FFT_vectorized(x):
     return X.ravel()
 
 data = read_from_excel_file('sample.xlsx', 'For C8 between lines')
-print(data)
+x_coordinates = list(map(lambda p: p['x'], data))
+min_x, max_x, points_count = min(x_coordinates), max(x_coordinates), len(x_coordinates)
+print(min_x, max_x, points_count)
+
+y = [p['y'] for p in data]
+
+result = FFT_vectorized(np.asarray(y))
+print(result)
