f0e87a646b44fb60540a2f2ce3dd70ec69582d3c
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2022-12-03 17:51:51 +0600

    feat: start working on interpolation logic

--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 import xlrd
 import numpy as np
 
-def read_from_excel_file(Workbook_path, sheet_name):
-    book = xlrd.open_workbook(Workbook_path)
+DATASETS = [
+    {'workbook_path': 'sample.xlsx', 'sheet_name': 'For C8 between lines'}
+]
+
+def read_from_excel_file(workbook_path, sheet_name):
+    book = xlrd.open_workbook(workbook_path)
     sheet = book.sheet_by_name(sheet_name)
     return [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
 
@@ -12,16 +16,20 @@ def remove_rows(data):
     return data[1:]
 
 def remove_columns(data):
-    return list(map(lambda row: {'x': row[0], 'y': row[1]}, data))
+    return [row[:2] for row in data]
+
+def convert_to_floats_matrix(data):
+    return [{'x': float(row[0]), 'y': float(row[1])} for row in data]
 
 def average_by_x_coordinates(data):
-    data_grouped_by_x_coordinates = {x: [] for x in data.keys()}
-    for x, y in data:
-        data_grouped_by_x_coordinates[x].append(y)
-    return {x: 0 if len(y_values) == 0 else sum(y_values) / len(y_values) for x, y_values in data.values()}
+    data_grouped_by_x_coordinates = {}
+    for row in data:
+        if not(row['x'] in data_grouped_by_x_coordinates.keys()): data_grouped_by_x_coordinates[row['x']] = []
+        data_grouped_by_x_coordinates[row['x']].append(row['y'])
+    return {x: 0 if len(y_values) == 0 else sum(y_values) / len(y_values) for x, y_values in data_grouped_by_x_coordinates.items()}
 
 def sort_by_x_coordinates(data):
-    return sorted(data, key=lambda d: d['x'])
+    return {key: data[key] for key in sorted(data.keys())}
 
 def fft(x):
     x = np.asarray(x, dtype=float)
@@ -47,17 +55,27 @@ def fft(x):
 
     return X.ravel()
 
-data = read_from_excel_file('sample.xlsx', 'For C8 between lines')
-data = remove_rows(data)
-data = remove_columns(data)
-data = average_by_x_coordinates(data)
-data = sort_by_x_coordinates(data)
+for dataset in DATASETS:
+    data = read_from_excel_file(dataset['workbook_path'], dataset['sheet_name'])
+    data = remove_rows(data)
+    data = remove_columns(data)
+    data = convert_to_floats_matrix(data)
+    data = average_by_x_coordinates(data)
+    data = sort_by_x_coordinates(data)
 
-x_coordinates = list(map(lambda p: p['x'], data))
-min_x, max_x, points_count = min(x_coordinates), max(x_coordinates), len(x_coordinates)
-print(min_x, max_x, points_count)
+    x_coordinates = data.keys()
+    min_x, max_x, points_count = min(x_coordinates), max(x_coordinates), len(x_coordinates)
 
-y = [p['y'] for p in data]
+    y_matrix_size = 1
+    while y_matrix_size < points_count:
+        y_matrix_size *= 2
 
-result = fft(np.asarray(y))
-print(result)
+    y = []
+    x = min_x
+    pointer = 0
+    step = (max_x - min_x) / (y_matrix_size - 1)
+    for i in range(y_matrix_size):
+        x += step
+
+    # result = fft(np.asarray(y))
+    # print(result)
