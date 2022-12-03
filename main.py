b9519b587b9e12037aa859fb75a876205bf8338e
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2022-12-03 10:10:13 +0600

    ref: use separate function for reading from excel file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+# coding=utf-8
+
 import xlrd
-book = xlrd.open_workbook('sample.xlsx')
-sheet = book.sheet_by_name('For C8 between lines')
-data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+
+def __normalize_excel_data_from_file(data):
+    return list(map(lambda row: row[:2], data))
+
+def read_from_excel_file(Workbook_path, sheet_name):
+    book = xlrd.open_workbook(Workbook_path)
+    sheet = book.sheet_by_name(sheet_name)
+    data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+    return __normalize_excel_data_from_file(data)
+
+data = read_from_excel_file('sample.xlsx', 'For C8 between lines')
 print(data)
